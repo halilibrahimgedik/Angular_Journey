58c36d9d172a71c78b0829a1fb654e6f9147ab98
Halil Gedik <halilgedik4234@gmail.com>
2024-03-25 09:19:48 +0300

    learning event binding

---
+++ Password App/1-Event Binding/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'passwordApp';
+  name = 'halil gedik';
+  buttonClick(){
+    console.log("Hello World, I'm learning 'Event Binding' ");
+  }
+}
